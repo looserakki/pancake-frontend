3db8439c3929e1df681d16ddad3866f6fb677cbd
looserakki <70113298+looserakki@users.noreply.github.com>
2021-10-22 06:20:54 +0530

    Update Header.tsx

--- src/views/ProfileCreation/Header.tsx
+++ src/views/ProfileCreation/Header.tsx
@@ -24,7 +24,7 @@ const Header: React.FC = () => {
         {t('Profile Setup')}
       </Heading>
       <Heading as="h2" scale="lg" mb="8px">
-        {t('Show off your stats and collectibles with your unique profile')}
+        {t('Show off your stats and collectibles with your unique profile DeCode')}
       </Heading>
       <Text color="textSubtle" mb="8px">
         {t('Total cost: 1.5 CAKE')}
